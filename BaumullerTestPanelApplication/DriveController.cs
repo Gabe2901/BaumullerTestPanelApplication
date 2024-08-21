f9857d3bd094fa73984b4ec25adce514f71a2878
Gabe <thorleygabe@outlook.com>
2024-08-21 10:09:08 +0100

    Added plc control class and currently have a working drive control for the test drive setup

--- BaumullerTestPanelApplication/DriveController.cs
+++ BaumullerTestPanelApplication/DriveController.cs
@@ -1,7 +1,23 @@
-﻿namespace BaumullerTestPanelApplication
+﻿using System;
+using S7.Net;
+
+namespace BaumullerTestPanelApplication
 {
     internal class DriveController
     {
         //TODO: setup the drive controller
+        public void DriveStart(string DriveTag, Plc PLC)
+        {
+            PLC.WriteBit(DataType.Memory, 0, 0, 0, true);
+        }
+
+        public void DriveStop(string DriveTag, Plc PLC)
+        {
+            PLC.WriteBit(DataType.Memory, 0, 0, 0, false);
+        }
+        public int GetDriveNumberFromTag(string DriveTag)
+        {
+            return Convert.ToInt32(DriveTag.Substring(DriveTag.Length - 1));
+        }
     }
 }
